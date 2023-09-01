51fc60cdead4b1aa5cd4681ee77c9ca3691cdee6
Naman Gupta <namanguptaprogrammer@gmail.com>
2023-09-01 12:54:49 +0530

    Added Templates

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Home Page"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug= True)
